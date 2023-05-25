6b5cd0cbaba98fdaf535ee83e5e4889f3fa1abb1
TFLGabriel <gabriel.tarozzo@hotmail.com>
2023-05-25 17:15:03 -0300

    Correcoes de estilo

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -5,30 +5,47 @@
 @endsection
 
 @section('content')
-    <div class="container">
+    <body class="nav-fixed">
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <!-- Conteúdo do menu lateral -->
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container">
 
-<h1>Detalhes do Contato</h1>
+                        <h1>Detalhes do Contato</h1>
 
-@if (session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-@endif
+                        @if (session('success'))
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                        @endif
 
-<p><strong>Nome:</strong> {{ $contact->name }}</p>
-<p><strong>Contato:</strong> {{ $contact->contact }}</p>
-<p><strong>Email:</strong> {{ $contact->email }}</p>
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Informações do Contato</h5>
+                                <p class="card-text"><strong>Nome:</strong> {{ $contact->name }}</p>
+                                <p class="card-text"><strong>Contato:</strong> {{ $contact->contact }}</p>
+                                <p class="card-text"><strong>Email:</strong> {{ $contact->email }}</p>
+                            </div>
+                        </div>
 
-<a href="{{ route('contacts.edit', $contact->id) }}">Editar</a>
+                        <div class="mt-4">
+                            <a href="{{ route('contacts.edit', $contact->id) }}" class="btn btn-primary">Editar</a>
 
-<form action="{{ route('contacts.destroy', $contact->id) }}" method="POST">
-    @csrf
-    @method('DELETE')
-    <button type="submit">Excluir</button>
-</form>
+                            <form action="{{ route('contacts.destroy', $contact->id) }}" method="POST" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Excluir</button>
+                            </form>
 
-<a href="{{ route('contacts.index') }}" class="btn btn-primary">Voltar</a>
+                            <a href="{{ route('contacts.index') }}" class="btn btn-secondary">Voltar</a>
+                        </div>
 
-</div>
-
+                    </div>
+                </main>
+            </div>
+        </div>
+    </body>
 @endsection
