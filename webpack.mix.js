c2ef848e716b7c6fb42fc6a7d92a9730e6287f16
TFLGabriel <gabriel.tarozzo@hotmail.com>
2023-05-25 16:59:07 -0300

    webpack correcao2

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,11 +12,12 @@ const mix = require('laravel-mix');
  */
 
  mix.js('resources/js/app.js', 'public/js')
-     .js('resources/js/scripts.js', 'public/js')
+     .copyDirectory('resources/js/', 'public/js')
      .js('resources/js/datatables/datatables-simple-demo.js', 'public/js')
      .postCss('resources/css/app.css', 'public/css', [
          //
      ])
      .sass('node_modules/bootstrap/scss/bootstrap.scss', 'public/css')
-     .sass('resources/sass/style.scss', 'public/css')
-     .copy('resources/img/favicon.png', 'public/img');
+     .copyDirectory('resources/css/', 'public/css')
+     .copyDirectory('resources/sass/', 'public/scss')
+     .copyDirectory('resources/assets', 'public/assets');
