850a069e750fd4ffe79d5e42a6a24d9f393f1555
TFLGabriel <gabriel.tarozzo@hotmail.com>
2023-05-25 16:06:08 -0300

    feat: NPM e cards de estilos e afins
    - NPM e cards de estilos
    
    #7

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
+
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ])
+    .sass('node_modules/bootstrap/scss/bootstrap.scss', 'public/css');
