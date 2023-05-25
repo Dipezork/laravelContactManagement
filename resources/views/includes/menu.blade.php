6066a74861376e121ff2ced7a1f76fd56b04dd1a
TFLGabriel <gabriel.tarozzo@hotmail.com>
2023-05-25 17:50:29 -0300

    Correcoes de estilo

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -7,13 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Overview - SB Admin Pro</title>
-    <link href="css/styles.css" rel="stylesheet" />
-    <link rel="icon" type="image/x-icon" href="assets/img/favicon.png" />
-    <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous">
-    </script>
+    <title>Teste Laravel</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.png') }}" />
+
 </head>
 
 <body class="nav-fixed">
@@ -288,7 +287,7 @@
                             </nav>
                         </div>
 
-
+                        <br>
                         <!-- Sidenav Footer-->
                         <div class="sidenav-footer">
                             <div class="sidenav-footer-content">
@@ -299,8 +298,11 @@
                                 @else
                                     <div class="text-danger">Não é um usuário autenticado (Sem permissões)</div>
                                 @endif
+
                             </div>
+
                         </div>
+
             </nav>
         </div>
 
@@ -320,7 +322,14 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
-    <script src="js/scripts.js"></script>
+    <script src="{{ asset('js/scripts.js') }}"></script>
+    <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+
+    <script src="{{ asset('js/datatables/datatables-simple-demo.js') }}"></script>
 </body>
 
 </html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,20 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
-    <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.png') }}" />
-    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
-    </script>
-    <script src="{{ asset('js/scripts.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/datatables/datatables-simple-demo.js') }}"></script>
-
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
