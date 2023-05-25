78579382f1d8e6eb4482dad1ced88bfb816e4d05
TFLGabriel <gabriel.tarozzo@hotmail.com>
2023-05-25 17:14:49 -0300

    Correcoes de estilo

--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -5,33 +5,52 @@
 @endsection
 
 @section('content')
-    <div class="container">
-
-        <h1>Editar Contato</h1>
-
-        <form action="{{ route('contacts.update', $contact->id) }}" method="POST">
-            @csrf
-            @method('PUT')
-
-            <div>
-                <label for="name">Nome:</label>
-                <input type="text" name="name" id="name" value="{{ $contact->name }}">
+    <body class="nav-fixed">
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <!-- Conteúdo do menu lateral -->
             </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container">
 
-            <div>
-                <label for="contact">Contato:</label>
-                <input type="text" name="contact" id="contact" value="{{ $contact->contact }}">
+                        <h1>Editar Contato</h1>
+
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Editar Informações do Contato</h5>
+
+                                <form action="{{ route('contacts.update', $contact->id) }}" method="POST">
+                                    @csrf
+                                    @method('PUT')
+
+                                    <div class="mb-3">
+                                        <label for="name" class="form-label">Nome:</label>
+                                        <input type="text" class="form-control" name="name" id="name" value="{{ $contact->name }}">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="contact" class="form-label">Contato:</label>
+                                        <input type="text" class="form-control" name="contact" id="contact" value="{{ $contact->contact }}">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="email" class="form-label">Email:</label>
+                                        <input type="email" class="form-control" name="email" id="email" value="{{ $contact->email }}">
+                                    </div>
+
+                                    <div class="mt-8">
+                                    <button type="submit" class="btn btn-primary">Salvar</button>
+                                    <a href="{{ route('contacts.show', $contact->id) }}" class="btn btn-secondary">Voltar</a>
+                                </div>
+                                </form>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </main>
             </div>
-
-            <div>
-                <label for="email">Email:</label>
-                <input type="email" name="email" id="email" value="{{ $contact->email }}">
-            </div>
-
-            <button type="submit">Salvar</button>
-        </form>
-
-        <a href="{{ route('contacts.show', $contact->id) }}" class="btn btn-primary">Voltar</a>
-
-    </div>
+        </div>
+    </body>
 @endsection
